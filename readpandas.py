08edf091f0c4992fbfba8603418907e4b999d7e0
Александр Занин <Zanin.a@sk-design.local>
2020-09-08 09:19:50 +0300

    Рабочий вариант

--- readpandas.py
+++ readpandas.py
@@ -12,50 +12,40 @@ class DateTimeEncoder(JSONEncoder):
     """
     Перезаписываем формат даты
     """
-
     def default(self, obj):
         if isinstance(obj, (datetime.date, datetime.datetime)):
             return obj.isoformat()
 
 
+""" Соединяемся с базой """
 sqlEngine = create_engine('mysql+pymysql://root:root@127.0.0.1/dump_origin', pool_recycle=3600)
 dbConnection = sqlEngine.connect()
-
-frame = pd.read_sql("SELECT * FROM view_products", dbConnection)
-
-
+""" Делаем запрос к таблице """
+data_from_bd_wordpress = pd.read_sql("SELECT * FROM view_products", dbConnection)
 pd.set_option('display.expand_frame_repr', False)
 
-df = pd.DataFrame(frame)
-result = df.to_json(orient='table', index=False, force_ascii=False)
+df = pd.DataFrame(data_from_bd_wordpress)
+""" Без force_ascii следующая строка encode и decode не работает """
+string_result_json_data = df.to_json(orient='table', index=False, force_ascii=False)
+""" Кириллица в базе была закодирована """
+result_read_string_json = json.loads(string_result_json_data.encode('cp866').decode('utf8'))
 
-parser = json.loads(result.encode('cp866').decode('utf8'))
-
-
+""" Читаем наш json-файл с раскодированными данными """
 with open("myjsonp.json", "w", encoding='utf-8') as file:
-    json.dump(parser, file, ensure_ascii=False, cls=DateTimeEncoder)
-
-
-dp = json_normalize(parser['data'])
-print(dp)
-
-# yyy = pd.DataFrame(kkk)
+    json.dump(result_read_string_json, file, ensure_ascii=False, cls=DateTimeEncoder)
+""" Производим нормализацию JSON, так как в нем есть лишние данные """
+json_data_normalize = json_normalize(result_read_string_json['data'])
+""" Таблица, в которую переносим нормализированные данные """
 tableName = "wp_posts_new"
 
 try:
-    frame = dp.to_sql(tableName, dbConnection, if_exists='replace')
-
+    """ Если таблица существует, то перезаписываем данные в ней """
+    frame = json_data_normalize.to_sql(tableName, dbConnection, if_exists='replace')
 except ValueError as vx:
-
     print(vx)
-
 except Exception as ex:
-
     print(ex)
-
 else:
-
-    print("Table %s created successfully." % tableName)
-
+    print("Таблица успешно создана: " % tableName)
 finally:
     dbConnection.close()
